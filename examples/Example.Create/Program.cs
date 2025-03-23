925a20e004d83606f2298ad8c765d7840e7f9806
TakWolf <takwolf@foxmail.com>
2025-03-24 02:20:02 +0800

    添加示例

---
+++ examples/Example.Create/Program.cs
@@ -0,0 +1,67 @@
+﻿using BdfSpec;
+
+var projectRootDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "..", ".."));
+var outputsDir = Path.Combine(projectRootDir, "build", "create");
+
+if (Directory.Exists(outputsDir))
+{
+    Directory.Delete(outputsDir, true);    
+}
+Directory.CreateDirectory(outputsDir);
+
+var font = new BdfFont(
+    pointSize: 16, 
+    resolution: (75, 75),
+    boundingBox: (16, 16, 0, -2));
+
+font.Glyphs.Add(new BdfGlyph(
+    name: "A",
+    encoding: 65,
+    scalableWidth: (500, 0),
+    deviceWidth: (8, 0),
+    boundingBox: (8, 16, 0, -2),
+    bitmap: [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ]));
+
+font.Properties.Foundry = "Pixel Font Studio";
+font.Properties.FamilyName = "My Font";
+font.Properties.WeightName = "Medium";
+font.Properties.Slant = "R";
+font.Properties.SetWidthName = "Normal";
+font.Properties.AddStyleName = "Sans Serif";
+font.Properties.PixelSize = font.PointSize;
+font.Properties.PointSize = font.PointSize * 10;
+font.Properties.ResolutionX = font.ResolutionX;
+font.Properties.ResolutionY = font.ResolutionY;
+font.Properties.Spacing = "P";
+font.Properties.AverageWidth = Convert.ToInt32(Math.Round(font.Glyphs.Sum(glyph => glyph.DeviceWidthX * 10) / Convert.ToDouble(font.Glyphs.Count)));
+font.Properties.CharsetRegistry = "ISO10646";
+font.Properties.CharsetEncoding = "1";
+font.GenerateNameAsXlfd();
+
+font.Properties.DefaultChar = -1;
+font.Properties.FontAscent = 14;
+font.Properties.FontDescent = 2;
+font.Properties.XHeight = 7;
+font.Properties.CapHeight = 10;
+
+font.Properties.FontVersion = "1.0.0";
+font.Properties.Copyright = "Copyright (c) TakWolf";
+
+font.Save(Path.Combine(outputsDir, "my-font.bdf"));
---
+++ examples/Example.Load/Program.cs
@@ -0,0 +1,32 @@
+﻿using BdfSpec;
+
+var projectRootDir = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "..", ".."));
+var assetsDir = Path.Combine(projectRootDir, "assets");
+var outputsDir = Path.Combine(projectRootDir, "build", "load");
+
+if (Directory.Exists(outputsDir))
+{
+    Directory.Delete(outputsDir, true);    
+}
+Directory.CreateDirectory(outputsDir);
+
+var font = BdfFont.Load(Path.Combine(assetsDir, "unifont", "unifont-16.0.02.bdf"));
+Console.WriteLine($"name: {font.Name}");
+Console.WriteLine($"size: {font.PointSize}");
+Console.WriteLine($"ascent: {font.Properties.FontAscent}");
+Console.WriteLine($"descent: {font.Properties.FontDescent}");
+Console.WriteLine();
+foreach (var glyph in font.Glyphs)
+{
+    Console.WriteLine($"char: {char.ConvertFromUtf32(glyph.Encoding)} ({glyph.Encoding:X4})");
+    Console.WriteLine($"glyphName: {glyph.Name}");
+    Console.WriteLine($"advanceWidth: {glyph.DeviceWidthX}");
+    Console.WriteLine($"dimensions: {glyph.Dimensions}");
+    Console.WriteLine($"offset: {glyph.Offset}");
+    foreach (var text in glyph.Bitmap.Select(bitmapRow => string.Join("", bitmapRow).Replace("0", "  ").Replace("1", "██")))
+    {
+        Console.WriteLine($"{text}*");
+    }
+    Console.WriteLine();
+}
+font.Save(Path.Combine(outputsDir, "unifont-16.0.02.bdf"));
