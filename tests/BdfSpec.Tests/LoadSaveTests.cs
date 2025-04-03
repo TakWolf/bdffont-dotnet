ddda0ad1c154d7525f93329f671ae4430bc0db0e
TakWolf <takwolf@foxmail.com>
2025-04-03 10:18:57 +0800

    修复 unifont 的兼容测试用例

--- tests/BdfSpec.Tests/LoadSaveTests.cs
+++ tests/BdfSpec.Tests/LoadSaveTests.cs
@@ -8,8 +8,8 @@ public class LoadSaveTests
         var loadPath = Path.Combine("assets", "unifont", "unifont-16.0.02.bdf");
         var savePath = Path.Combine(PathUtils.CreateTempDir(), "unifont-16.0.02.bdf");
         var font = BdfFont.Load(loadPath);
-        File.WriteAllText(savePath, font.DumpToString().Replace("\nBITMAP\n", "\nBITMAP \n"));
-        Assert.Equal(File.ReadAllText(loadPath).Replace("\r\n", "\n"), File.ReadAllText(savePath));
+        font.Save(savePath);
+        Assert.Equal(File.ReadAllText(loadPath).Replace("\r\n", "\n"), File.ReadAllText(savePath).Replace("\nBITMAP\n", "\nBITMAP \n"));
     }
 
     [Fact]
@@ -18,8 +18,8 @@ public class LoadSaveTests
         var loadPath = Path.Combine("assets", "unifont", "unifont-16.0.02.bdf");
         var savePath = Path.Combine(PathUtils.CreateTempDir(), "unifont-16.0.02.bdf");
         var font = await BdfFont.LoadAsync(loadPath);
-        await File.WriteAllTextAsync(savePath, (await font.DumpToStringAsync()).Replace("\nBITMAP\n", "\nBITMAP \n"));
-        Assert.Equal((await File.ReadAllTextAsync(loadPath)).Replace("\r\n", "\n"), await File.ReadAllTextAsync(savePath));
+        await font.SaveAsync(savePath);
+        Assert.Equal((await File.ReadAllTextAsync(loadPath)).Replace("\r\n", "\n"), (await File.ReadAllTextAsync(savePath)).Replace("\nBITMAP\n", "\nBITMAP \n"));
     }
 
     [Fact]
